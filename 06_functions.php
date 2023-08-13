8d7e19667104072baf02d1bf7dae35afa439e5ed
Aboullah Balde <abdoullahbalde@gmail.com>
2023-08-13 21:29:45 +0200

    commit fonction php

---
+++ 06_functions.php
@@ -0,0 +1,49 @@
+<?php
+
+// $x = 10;
+
+// function greetings()
+// {
+//     global $x;
+//     echo $x . "<br>";
+//     echo "salut les pros";
+// }
+
+// echo $x . '<br>';
+// greetings();
+
+// les parametres d'une fonction
+
+// function greetings($nom, $prenom)
+// {
+//     echo "salut $nom $prenom";
+// }
+
+// greetings('omar', 'kamdjou ');
+
+// greetings("jafar", 'kejd');
+
+// fonction anonyme
+// $salutation = function ($p1, $p2) {
+//     echo "salut $p1 $p2";
+// };
+
+// $salutation('dev', 'code');
+
+// fonction fleches
+// $salutation = fn () => "Salut les pros..";
+
+// echo $salutation();
+
+// les return function
+
+// function addition($n1, $n2)
+// {
+//     return $n1 + $n2;
+// }
+
+// echo addition(45, 44);
+
+// $substraction = fn ($n1, $n2) => $n1 - $n2;
+// $sub = $substraction(45, 5);
+// echo $sub;
